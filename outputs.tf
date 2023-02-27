d1011c241cca2bb9c3a3b695819fe2621e2e4ae6
Samuel Lee <33992350+samuellee-zy@users.noreply.github.com>
2023-02-27 13:34:10 +1100

    Edited outputs

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,21 @@
-output "application_a_security_workspace_id" {
-  value = tfe_workspace.application_a_security.id
+output "hcp_vault_deployment_id" {
+  value = tfe_workspace.hcp-vault-deployment.id
 }
 
-output "application_a_vnet_workspace_id" {
-  value = tfe_workspace.application_a_vnet.id
+output "hcp_boundary_deployment_id" {
+  value = tfe_workspace.hcp-boundary-deployment.id
+}
+
+output "hcp_consul_deployment_id" {
+  value = tfe_workspace.hcp-consul-deployment.id
+}
+
+output "hcp_waypoint_deployment_id" {
+  value = tfe_workspace.hcp-waypoint-deployment.id
+}
+
+output "hcp_packer_deployment_id" {
+  value = tfe_workspace.hcp-packer-deployment.id
 }
 
 # output "application_a_web_workspace_id" {
