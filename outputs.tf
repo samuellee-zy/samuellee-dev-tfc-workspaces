168cfcaa7c31ddf94652502cfd39a8ba2e1ad6b9
Samuel Lee <33992350+samuellee-zy@users.noreply.github.com>
2023-02-27 12:48:28 +1100

    Removed output for app_a_web_workspace

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,6 @@ output "application_a_vnet_workspace_id" {
   value = tfe_workspace.application_a_vnet.id
 }
 
-output "application_a_web_workspace_id" {
-  value = tfe_workspace.application_a_web.id
-}
+# output "application_a_web_workspace_id" {
+#   value = tfe_workspace.application_a_web.id
+# }
