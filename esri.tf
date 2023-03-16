196a9815b1a951dc7389a7bb4c0df93c5b9f9d87
Samuel Lee <33992350+samuellee-zy@users.noreply.github.com>
2023-03-16 11:14:52 +1100

    Minor Updates

--- esri.tf
+++ esri.tf
@@ -6,10 +6,10 @@ resource "tfe_workspace" "esri-ws" {
   execution_mode      = "remote"
   assessments_enabled = true
   project_id          = data.tfe_outputs.samuellee-dev-project-outputs.values.project_customer_demos.id
-  #   vcs_repo {
-  #     identifier         = "samuellee-zy/customer-ansarada"
-  #     branch             = "main"
-  #     ingress_submodules = false
-  #     oauth_token_id     = data.tfe_oauth_client.samuellee-dev-tfc-github-client.oauth_token_id
-  #   }
+  vcs_repo {
+    identifier         = "samuellee-zy/customer-esri"
+    branch             = "main"
+    ingress_submodules = false
+    oauth_token_id     = data.tfe_oauth_client.samuellee-dev-tfc-github-client.oauth_token_id
+  }
 }
