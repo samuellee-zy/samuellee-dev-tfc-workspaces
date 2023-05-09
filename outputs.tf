d55242f5c0480b1ab5f242d45ddbdc610fb40c9b
Samuel Lee <33992350+samuellee-zy@users.noreply.github.com>
2023-05-09 10:29:48 +1000

    Removed workspaces

--- outputs.tf
+++ outputs.tf
@@ -18,13 +18,13 @@ output "hcp_packer_deployment_id" {
   value = tfe_workspace.hcp-packer-deployment.id
 }
 
-output "ansarada_ws_id" {
-  value = tfe_workspace.ansarada-ws.id
-}
+# output "ansarada_ws_id" {
+#   value = tfe_workspace.ansarada-ws.id
+# }
 
-output "esri_ws_id" {
-  value = tfe_workspace.esri-ws.id
-}
+# output "esri_ws_id" {
+#   value = tfe_workspace.esri-ws.id
+# }
 
 # output "application_a_web_workspace_id" {
 #   value = tfe_workspace.application_a_web.id
