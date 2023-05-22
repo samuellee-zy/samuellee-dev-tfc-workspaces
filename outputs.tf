e3bc1dd9c96adae4fe02ff08bfb95fd16366aaf4
Samuel Lee <33992350+samuellee-zy@users.noreply.github.com>
2023-05-22 19:07:00 +1000

    updated hcpv tf config

--- outputs.tf
+++ outputs.tf
@@ -1,22 +1,22 @@
-output "hcp_vault_deployment_id" {
-  value = tfe_workspace.hcp-vault-deployment.id
-}
+# output "hcp_vault_deployment_id" {
+#   value = tfe_workspace.hcp-vault-deployment.id
+# }
 
-output "hcp_boundary_deployment_id" {
-  value = tfe_workspace.hcp-boundary-deployment.id
-}
+# output "hcp_boundary_deployment_id" {
+#   value = tfe_workspace.hcp-boundary-deployment.id
+# }
 
-output "hcp_consul_deployment_id" {
-  value = tfe_workspace.hcp-consul-deployment.id
-}
+# output "hcp_consul_deployment_id" {
+#   value = tfe_workspace.hcp-consul-deployment.id
+# }
 
-output "hcp_waypoint_deployment_id" {
-  value = tfe_workspace.hcp-waypoint-deployment.id
-}
+# output "hcp_waypoint_deployment_id" {
+#   value = tfe_workspace.hcp-waypoint-deployment.id
+# }
 
-output "hcp_packer_deployment_id" {
-  value = tfe_workspace.hcp-packer-deployment.id
-}
+# output "hcp_packer_deployment_id" {
+#   value = tfe_workspace.hcp-packer-deployment.id
+# }
 
 # output "ansarada_ws_id" {
 #   value = tfe_workspace.ansarada-ws.id
@@ -29,3 +29,7 @@ output "hcp_packer_deployment_id" {
 # output "application_a_web_workspace_id" {
 #   value = tfe_workspace.application_a_web.id
 # }
+
+output "hcpv_plus_controlGroupDemo" {
+  value = tfe_workspace.hcpv_plus_controlGroupDemo.id
+}
