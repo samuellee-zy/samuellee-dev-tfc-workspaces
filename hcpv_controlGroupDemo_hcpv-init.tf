706e2b86364ee6c68ff9609ac275eee704a048fe
Samuel Lee <33992350+samuellee-zy@users.noreply.github.com>
2023-05-22 18:48:28 +1000

    Added new workspace

---
+++ hcpv_controlGroupDemo_hcpv-init.tf
@@ -0,0 +1,9 @@
+resource "tfe_workspace" "hcp-vault-deployment" {
+  name                = "hcpv-plus-controlGroupDemo"
+  organization        = var.orgName
+  tag_names           = ["hcp", "vault", "deployment"]
+  description         = "HCP Vault Cluster Plus Tier Deployment"
+  execution_mode      = "remote"
+  assessments_enabled = true
+  project_id          = data.tfe_outputs.samuellee-dev-project-outputs.values.project_hcp_vault.id
+}
