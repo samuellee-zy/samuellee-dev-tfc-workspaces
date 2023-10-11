df2a5ff4c093824e032110c9c2b21c8f72f2bb70
Samuel Lee <33992350+samuellee-zy@users.noreply.github.com>
2023-10-11 15:30:22 +1100

    updated terraform workspaces

--- Demos-workspaces.tf
+++ Demos-workspaces.tf
@@ -1,7 +1,7 @@
 resource "tfe_workspace" "cicdDemo" {
   name                = "tfc-cicd-demo"
   organization        = var.orgName
-  tag_names           = ["aws", "api", "cicd"]
+  tag_names           = ["aws", "cicd"]
   description         = "CICD - API Driven Workflow Demo"
   execution_mode      = "remote"
   assessments_enabled = true
@@ -20,11 +20,70 @@ resource "tfe_workspace_run_task" "cicdDemo-run-task" {
   stage             = "post_plan"
 }
 
-resource "tfe_workspace_policy_set" "infracost" {
+resource "tfe_workspace_policy_set" "cicd-infracost" {
   policy_set_id = data.tfe_policy_set.infracost.id
-  workspace_id = tfe_workspace.cicdDemo.id
+  workspace_id  = tfe_workspace.cicdDemo.id
 }
 
+////// VCS DEMO //////////
+resource "tfe_workspace" "vcsDemo" {
+  name                = "tfc-vcs-demo"
+  organization        = var.orgName
+  tag_names           = ["aws", "vcs"]
+  description         = "VCS - Version Control System Workflow Demo"
+  execution_mode      = "remote"
+  assessments_enabled = true
+  project_id          = data.tfe_outputs.samuellee-dev-project-outputs.values.project_Demos.id
+  vcs_repo {
+    identifier     = "samuellee-zy/tfc-demo"
+    oauth_token_id = "ot-r2UAuuHJWYiiY6Uv"
+  }
+}
+
+resource "tfe_workspace_variable_set" "vcsDrivenWorkflow-aws-dynamic-creds-varset" {
+  workspace_id    = tfe_workspace.vcsDemo.id
+  variable_set_id = data.tfe_outputs.samuellee-dev-tfc-dynamic-creds-varset-outputs.values.dynamic_creds_varset_id
+}
+
+resource "tfe_workspace_run_task" "vcsDemo-run-task" {
+  workspace_id      = tfe_workspace.vcsDemo.id
+  task_id           = data.tfe_organization_run_task.infracost.id
+  enforcement_level = "advisory"
+  stage             = "post_plan"
+}
+
+resource "tfe_workspace_policy_set" "vcs-infracost" {
+  policy_set_id = data.tfe_policy_set.infracost.id
+  workspace_id  = tfe_workspace.vcsDemo.id
+}
+
+resource "tfe_variable" "demoTag" {
+  key          = "demo_tag"
+  value        = "aws_immersion_day"
+  category     = "terraform"
+  workspace_id = tfe_workspace.vcsDemo.id
+}
+
+resource "tfe_variable" "instanceName" {
+  key          = "instance_name"
+  value        = "demoInstance"
+  category     = "terraform"
+  workspace_id = tfe_workspace.vcsDemo.id
+}
+
+resource "tfe_variable" "instanceType" {
+  key          = "instance_type"
+  value        = "t2.2xlarge"
+  category     = "terraform"
+  workspace_id = tfe_workspace.vcsDemo.id
+}
+
+resource "tfe_variable" "region" {
+  key          = "region"
+  value        = "ap-southeast-1"
+  category     = "terraform"
+  workspace_id = tfe_workspace.vcsDemo.id
+}
 # resource "tfe_workspace" "hcpv-plus-controlGroupDemo" {
 #   name                = "hcpv-plus-controlGroupDemo"
 #   organization        = var.orgName
